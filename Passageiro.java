0f514b45a20dfa9ba352648bd41881116ce5557c
Lug <gdom23@gmail.com>
2015-06-29 13:51:00 -0300

    Stuff!

---
+++ Passageiro.java
@@ -0,0 +1,10 @@
+public class Passageiro {
+	public int xi, yi, xf, yf;
+	
+	public Passageiro(int xi, int yi, int xf, int yf) {
+		this.xi = xi;
+		this.yi = yi;
+		this.xf = xf;
+		this.yf = yf;
+	}
+}
---
+++ Taxi.java
@@ -0,0 +1,10 @@
+public class Taxi {
+
+	public int x, y;
+	
+	public Taxi(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+}
